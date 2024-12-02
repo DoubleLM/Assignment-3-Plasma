4ce63b6e0008e99203307e4fff464a2e2cab1811
DoubleLM <laventuremiller@uleth.ca>
2024-12-02 05:44:54 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,389 @@
+console.log("INITIALIZING JQUERY");
+
+$("#cso1").on("click", function () {
+    console.log("Casper Scribe Outfit 1 clicked");
+    $(this).css("background-image", "url(imgs/cspixel1.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Casper Scribe Outfit 1 loaded");
+    $(this).unbind("click");
+});
+
+$("#cso2").on("click", function () {
+    console.log("Casper Scribe Outfit 2 clicked");
+    $(this).css("background-image", "url(imgs/cspixel2.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Casper Scribe Outfit 2 loaded");
+    $(this).unbind("click");
+});
+
+$("#cso3").on("click", function () {
+    console.log("Casper Scribe Outfit 3 clicked");
+    $(this).css("background-image", "url(imgs/cspixel3.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Casper Scribe Outfit 3 loaded");
+    $(this).unbind("click");
+});
+
+$("#cso4").on("click", function () {
+    console.log("Casper Scribe Outfit 4 clicked");
+    $(this).css("background-image", "url(imgs/cspixel4.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Casper Scribe Outfit 4 loaded");
+    $(this).unbind("click");
+});
+
+$("#cso5").on("click", function () {
+    console.log("Casper Scribe Outfit 5 clicked");
+    $(this).css("background-image", "url(imgs/cspixel5.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Casper Scribe Outfit 5 loaded");
+    $(this).unbind("click");
+});
+
+console.log("CASPER SCRIBE OUTFITS LOADED");
+
+$("#jwo1").on("click", function () {
+    console.log("Juniper Whitaker Outfit 1 clicked");
+    $(this).css("background-image", "url(imgs/jwpixel1.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Juniper Whitaker Outfit 1 loaded");
+    $(this).unbind("click");
+});
+
+$("#jwo2").on("click", function () {
+    console.log("Juniper Whitaker Outfit 2 clicked");
+    $(this).css("background-image", "url(imgs/jwpixel2.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Juniper Whitaker Outfit 2 loaded");
+    $(this).unbind("click");
+});
+
+$("#jwo3").on("click", function () {
+    console.log("Juniper Whitaker Outfit 3 clicked");
+    $(this).css("background-image", "url(imgs/jwpixel3.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Juniper Whitaker Outfit 3 loaded");
+    $(this).unbind("click");
+});
+
+$("#jwo4").on("click", function () {
+    console.log("Juniper Whitaker Outfit 4 clicked");
+    $(this).css("background-image", "url(imgs/jwpixel4.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Juniper Whitaker Outfit 4 loaded");
+    $(this).unbind("click");
+});
+
+$("#jwo5").on("click", function () {
+    console.log("Juniper Whitaker Outfit 5 clicked");
+    $(this).css("background-image", "url(imgs/jwpixel5.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Juniper Whitaker Outfit 5 loaded");
+    $(this).unbind("click");
+});
+
+console.log("JUNIPER WHITAKER OUTFITS LOADED");
+
+$("#ato1").on("click", function () {
+    console.log("Avian Tiercel Outfit 1 clicked");
+    $(this).css("background-image", "url(imgs/atpixel1.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Avian Tiercel Outfit 1 loaded");
+    $(this).unbind("click");
+});
+
+$("#ato2").on("click", function () {
+    console.log("Avian Tiercel Outfit 2 clicked");
+    $(this).css("background-image", "url(imgs/atpixel2.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Avian Tiercel Outfit 2 loaded");
+    $(this).unbind("click");
+});
+
+$("#ato3").on("click", function () {
+    console.log("Avian Tiercel Outfit 3 clicked");
+    $(this).css("background-image", "url(imgs/atpixel3.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Avian Tiercel Outfit 3 loaded");
+    $(this).unbind("click");
+});
+
+$("#ato4").on("click", function () {
+    console.log("Avian Tiercel Outfit 4 clicked");
+    $(this).css("background-image", "url(imgs/atpixel4.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Avian Tiercel Outfit 4 loaded");
+    $(this).unbind("click");
+});
+
+$("#ato5").on("click", function () {
+    console.log("Avian Tiercel Outfit 5 clicked");
+    $(this).css("background-image", "url(imgs/atpixel5.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Avian Tiercel Outfit 5 loaded");
+    $(this).unbind("click");
+});
+
+console.log("AVIAN TIERCEL OUTFITS LOADED");
+
+$("#aro1").on("click", function () {
+    console.log("Ardo Ruutel Outfit 1 clicked");
+    $(this).css("background-image", "url(imgs/arpixel1.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Ardo Ruutel Outfit 1 loaded");
+    $(this).unbind("click");
+});
+
+$("#aro2").on("click", function () {
+    console.log("Ardo Ruutel Outfit 2 clicked");
+    $(this).css("background-image", "url(imgs/arpixel2.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Ardo Ruutel Outfit 2 loaded");
+    $(this).unbind("click");
+});
+
+$("#aro3").on("click", function () {
+    console.log("Ardo Ruutel Outfit 3 clicked");
+    $(this).css("background-image", "url(imgs/arpixel3.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Ardo Ruutel Outfit 3 loaded");
+    $(this).unbind("click");
+});
+
+$("#aro4").on("click", function () {
+    console.log("Ardo Ruutel Outfit 4 clicked");
+    $(this).css("background-image", "url(imgs/arpixel4.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Ardo Ruutel Outfit 4 loaded");
+    $(this).unbind("click");
+});
+
+$("#aro5").on("click", function () {
+    console.log("Ardo Ruutel Outfit 5 clicked");
+    $(this).css("background-image", "url(imgs/arpixel5.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    $(this).html(" ");
+    console.log("Ardo Ruutel Outfit 5 loaded");
+    $(this).unbind("click");
+});
+
+console.log("ARDO RUUTEL OUTFITS LOADED");
+
+$("#rdo1").on("click", function () {
+    console.log("Raini Desdemona Outfit 1 clicked");
+    $(this).css("background-image", "url(imgs/rdpixel1.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Raini Desdemona  Outfit 1 loaded");
+    $(this).unbind("click");
+});
+
+$("#rdo2").on("click", function () {
+    console.log("Raini Desdemona Outfit 2 clicked");
+    $(this).css("background-image", "url(imgs/rdpixel2.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Raini Desdemona  Outfit 2 loaded");
+    $(this).unbind("click");
+});
+
+$("#rdo3").on("click", function () {
+    console.log("Raini Desdemona Outfit 3 clicked");
+    $(this).css("background-image", "url(imgs/rdpixel3.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Raini Desdemona Outfit 3 loaded");
+    $(this).unbind("click");
+});
+
+$("#rdo4").on("click", function () {
+    console.log("Raini Desdemona Outfit 4 clicked");
+    $(this).css("background-image", "url(imgs/rdpixel4.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Raini Desdemona Outfit 4 loaded");
+    $(this).unbind("click");
+});
+
+$("#rdo5").on("click", function () {
+    console.log("Raini Desdemona Outfit 5 clicked");
+    $(this).css("background-image", "url(imgs/rdpixel5.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Raini Desdemona Outfit 5 loaded");
+    $(this).unbind("click");
+});
+
+console.log("RAINI DESDEMONA OUTFITS LOADED");
+
+$("#iso1").on("click", function () {
+    console.log("Iraj Saharkhiz Outfit 1 clicked");
+    $(this).css("background-image", "url(imgs/ispixel1.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Iraj Saharkhiz Outfit 1 loaded");
+    $(this).unbind("click");
+});
+
+$("#iso2").on("click", function () {
+    console.log("Iraj Saharkhiz Outfit 2 clicked");
+    $(this).css("background-image", "url(imgs/ispixel2.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Iraj Saharkhiz Outfit 2 loaded");
+    $(this).unbind("click");
+});
+
+$("#iso3").on("click", function () {
+    console.log("Iraj Saharkhiz Outfit 3 clicked");
+    $(this).css("background-image", "url(imgs/ispixel3.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Iraj Saharkhiz Outfit 3 loaded");
+    $(this).unbind("click");
+});
+
+$("#iso4").on("click", function () {
+    console.log("Iraj Saharkhiz Outfit 4 clicked");
+    $(this).css("background-image", "url(imgs/ispixel4.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Iraj Saharkhiz Outfit 4 loaded");
+    $(this).unbind("click");
+});
+
+$("#iso5").on("click", function () {
+    console.log("Iraj Saharkhiz Outfit 5 clicked");
+    $(this).css("background-image", "url(imgs/ispixel5.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Iraj Saharkhiz Outfit 5 loaded");
+    $(this).unbind("click");
+});
+
+console.log("IRAJ SAHARKHIZ OUTFITS LOADED");
+
+$("#amo1").on("click", function () {
+    console.log("Amame Tachibana Outfit 1 clicked");
+    $(this).css("background-image", "url(imgs/ampixel1.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Amame Tachibana Outfit 1 loaded");
+    $(this).unbind("click");
+});
+
+$("#amo2").on("click", function () {
+    console.log("Amame Tachibana Outfit 2 clicked");
+    $(this).css("background-image", "url(imgs/ampixel2.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Amame Tachibana Outfit 2 loaded");
+    $(this).unbind("click");
+});
+
+$("#amo3").on("click", function () {
+    console.log("Amame Tachibana Outfit 3 clicked");
+    $(this).css("background-image", "url(imgs/ampixel3.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Amame Tachibana Outfit 3 loaded");
+    $(this).unbind("click");
+});
+
+$("#amo4").on("click", function () {
+    console.log("Amame Tachibana Outfit 4 clicked");
+    $(this).css("background-image", "url(imgs/ampixel4.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Amame Tachibana Outfit 4 loaded");
+    $(this).unbind("click");
+});
+
+$("#amo5").on("click", function () {
+    console.log("Amame Tachibana Outfit 5 clicked");
+    $(this).css("background-image", "url(imgs/ampixel5.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Amame Tachibana Outfit 5 loaded");
+    $(this).unbind("click");
+});
+
+console.log("AMAME TACHIBANA OUTFITS LOADED");
+
+$("#kso1").on("click", function () {
+    console.log("Kane Shio Outfit 1 clicked");
+    $(this).css("background-image", "url(imgs/kspixel1.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Kane Shio Outfit 1 loaded");
+    $(this).unbind("click");
+});
+
+$("#kso2").on("click", function () {
+    console.log("Kane Shio Outfit 2 clicked");
+    $(this).css("background-image", "url(imgs/kspixel2.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Kane Shio Outfit 2 loaded");
+    $(this).unbind("click");
+});
+
+$("#kso3").on("click", function () {
+    console.log("Kane Shio Outfit 3 clicked");
+    $(this).css("background-image", "url(imgs/kspixel3.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Kane Shio Outfit 3 loaded");
+    $(this).unbind("click");
+});
+
+$("#kso4").on("click", function () {
+    console.log("Kane Shio Outfit 4 clicked");
+    $(this).css("background-image", "url(imgs/kspixel4.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Kane Shio Outfit 4 loaded");
+    $(this).unbind("click");
+});
+
+$("#kso5").on("click", function () {
+    console.log("Kane Shio Outfit 5 clicked");
+    $(this).css("background-image", "url(imgs/kspixel5.png)");
+    $(this).css("background-size", "220px");
+    $(this).css("background-repeat", "no-repeat");
+    console.log("Kane Shio Outfit 5 loaded");
+    $(this).unbind("click");
+});
+
+console.log("KANE SHIO OUTFITS LOADED");
+
+console.log("ALL JQUERY LOADED. MISSION IS A GO, BOSS");
